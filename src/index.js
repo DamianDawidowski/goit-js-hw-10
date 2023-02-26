addc36c7967962ae1eb78150843e4e39b0752493
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-26 16:39:27 +0100

    deleted:    README.en.md 	modified:   README.md 	new file:   README.ru.md 	modified:   package-lock.json 	modified:   package.json 	new file:   src/css/styles.css 	new file:   src/fetchCountries.js 	deleted:    src/images/logo.png 	modified:   src/index.html 	modified:   src/index.js 	deleted:    src/partials/example.html 	deleted:    src/sass/_common.scss 	deleted:    src/sass/_example.scss 	deleted:    src/sass/index.scss

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+// import  './fetchCountries';
+const DEBOUNCE_DELAY = 500;
+
+  
+const input = document.querySelector('input');
+const countryList = document.querySelector('.country-list');
+const countryDetails = document.querySelector('.country-info');
+//  console.log(input);
+
+input.addEventListener(
+  'input',
+  debounce(() => {
+      
+       fetchCountries(`${input.value}`)
+         .then(users => renderUserList(users))
+         .catch(error => console.log(error));
+      
+      
+      
+   
+    //   console.log(`${input.value}`);
+      
+  }, DEBOUNCE_DELAY)
+);
+
+ function renderUserList(users) {
+   const markup = users
+       .map(user => {
+         console.log(`flag is: ${user.flags }`);
+       return `
+          <li class="listCountry"><p class="listName">${user.name}</p>
+          <img class="listFlag" width = 20 src=${user.flags.svg}   />
+         </li>
+      `;
+     })
+     .join('');
+   countryList.innerHTML = markup;
+ }
+console.log(`country list is: ${countryList}`);
+
+// fetchCountries('states');
+
+
+
+
+
+
+
+
+
+ 
